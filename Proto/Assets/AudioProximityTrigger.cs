97ee0026485f1aad2a5954008b4c5630505ade35
Boombostak <smokejaguar3050@hotmail.com>
2015-06-05 16:24:49 -0600

    Working toward mixer-based solution

---
+++ Proto/Assets/AudioProximityTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioProximityTrigger : MonoBehaviour {
+
+    public GameObject target;
+    public float sendlevel;
+    public AudioMixer mixer;
+    public string parameter_name;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MixReverb(this.transform.position, target.transform.position, parameter_name);
+	}
+
+    void MixReverb(Vector3 origin, Vector3 target, string parameter_name)
+    {
+        sendlevel = 1/Vector3.SqrMagnitude(target - origin);
+        mixer.SetFloat(parameter_name, sendlevel);
+    }
+}
