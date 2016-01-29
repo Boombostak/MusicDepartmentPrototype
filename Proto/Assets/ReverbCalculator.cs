4780d079e27e95f08bd4754e5d0032d9a4cb1e1e
Boombostak <smokejaguar3050@hotmail.com>
2016-01-29 11:50:59 -0700

    January Update for laptop

---
+++ Proto/Assets/ReverbCalculator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class ReverbCalculator : MonoBehaviour {
+
+	public AudioMixer mixer;
+	public AudioMixerSnapshot[] snapshots;
+	public GameObject[] nodes;
+	public float[] closenesses;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < nodes.Length; i++) {
+			closenesses[i] = 1/
+				Vector3.Distance(nodes[i].transform.position, 
+				                 GameObject.FindGameObjectWithTag("Player").transform.position);
+		}
+		BlendSnapshots ();
+	}
+
+	public void BlendSnapshots()
+	{
+		mixer.TransitionToSnapshots (snapshots, closenesses, 0.005f);
+	}
+}
