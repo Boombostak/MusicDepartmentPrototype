79236d497913e3a0fd51d227466ec12ac121e0e6
Boombostak <smokejaguar3050@hotmail.com>
2016-04-18 00:40:14 -0600

    Node Zones
    -Triggers not detecting rigidbody entry.

--- Proto/Assets/ReverbNodeZone.cs
+++ Proto/Assets/ReverbNodeZone.cs
@@ -1,24 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ReverbNodeZone : MonoBehaviour {
 
-	public Collider collider;
-	public GameObject[] nodeArray;
-	public GameObject player;
-	public Collision testToHit;
-	public Rigidbody rigidBody;
-	public float checkDistance;
-	public RaycastHit hit;
+	public Collider activeCollider;
+	public List<GameObject> nodeList;
+	public static GameObject[] allNodes;
 
 	//USE ONTRIGGERSTAY!!! Test if the player is staying in the trigger and adjust accordingly.
 
 	//for loop if the node is inside the collider it goes into the array of nodes associated with this collider. 
 	//If the player is inside the collider, the colliders node array becomes the players node array.
 
+
+	public void CalculateAssociatedNodes(){
+		int count;
+		for (int i = 0; i < allNodes.Length; i++) {
+			if (activeCollider.bounds.Contains(allNodes[i].transform.position)) {
+				nodeList.Add (allNodes [i]);
+			}
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		activeCollider = this.GetComponent<Collider> ();
+		allNodes = GameObject.FindGameObjectsWithTag("reverbnode");
+		CalculateAssociatedNodes ();
 	}
 	
 	// Update is called once per frame
---
+++ Proto/Assets/ReverbNodeZoneDetector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ReverbNodeZoneDetector : MonoBehaviour {
+
+	//PUT THIS ON THINGS THAT MAKE SOUNDS
+
+	public GameObject target;
+	public ReverbNodeZone rnz;
+	public List<GameObject> activeNodes;
+	// Use this for initialization
+	void Start () {
+		target = this.gameObject;
+	}
+	
+	void OnTriggerStay(Collider other){
+		Debug.Log ("triggerstay");
+		rnz = other.gameObject.GetComponent<ReverbNodeZone> ();
+		//activeNodes = new GameObject[rnz.nodeList.Count];
+		activeNodes = rnz.nodeList;
+	}
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("triggerenter");
+		rnz = other.gameObject.GetComponent<ReverbNodeZone> ();
+		//activeNodes = new GameObject[rnz.nodeList.Count];
+		activeNodes = rnz.nodeList;
+	}
+}
