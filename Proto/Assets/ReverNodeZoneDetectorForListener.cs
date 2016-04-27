1947d6ef82ce823776d3ae05301dcbf78fbbc6ab
Boombostak <smokejaguar3050@hotmail.com>
2016-04-27 13:38:16 -0600

    Room detection works, special case for listener

--- Proto/Assets/ReverNodeZoneDetectorForListener.cs
+++ Proto/Assets/ReverNodeZoneDetectorForListener.cs
@@ -3,20 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent(typeof(Rigidbody))]
-public class ReverbNodeZoneDetector : MonoBehaviour {
+public class ReverNodeZoneDetectorForListener : MonoBehaviour {
 
 	//PUT THIS ON THINGS THAT MAKE SOUNDS
 
 	public GameObject target;
 	public ReverbNodeZone rnz;
 	public List<GameObject> activeNodes;
-	public ReverbController targetController;
+	public ReverbControllerForListener targetController;
 	// Use this for initialization
 	void Start () {
 		target = this.gameObject;
-		targetController = target.GetComponent<ReverbController> ();
+		targetController = target.GetComponent<ReverbControllerForListener> ();
 	}
-	
+
 	void OnTriggerStay(Collider other){
 		Debug.Log ("triggerstay");
 		rnz = other.gameObject.GetComponent<ReverbNodeZone> ();
