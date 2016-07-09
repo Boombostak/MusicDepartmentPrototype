6d5bfb6e7fa87900114472a1ed50f4ff33f85fce
Boombostak <smokejaguar3050@hotmail.com>
2016-07-09 14:40:44 -0600

    Got DOTW tracker working
    -Debugs DOTW and those unique strings can be used for other triggering.

---
+++ Proto/Assets/SimpleCalendar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SimpleCalendar : MonoBehaviour {
+
+	public TOD_Sky Sky;
+	public string dotw;
+	public Text text;
+
+	public void CheckDay ()
+	{
+		text.text = Sky.Cycle.DateTime.DayOfWeek.ToString ();;
+	}
+
+	void Update ()
+	{
+		CheckDay ();
+	}
+}
+
+
+
