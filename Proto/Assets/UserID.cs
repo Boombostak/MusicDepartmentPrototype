d86fa63845cad8f2d46475d64bd28705f79d1000
Boombostak <smokejaguar3050@hotmail.com>
2016-09-13 14:10:58 -0600

    Cleanup

---
+++ Proto/Assets/UserID.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UserID : MonoBehaviour {
+
+	public string username;
+	public TextPopUpAndAlignToPlayer popup;
+	public Text name;
+	public InputField nameInputField;
+
+	// Use this for initialization
+	void Start () {
+		nameInputField = GameObject.Find ("NameInputField").GetComponent<InputField> ();
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nameInputField.transform.parent.GetComponent<Canvas>().enabled == false) {
+			name.text = nameInputField.text;
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+}
